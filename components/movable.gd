829e4f9677019a6c833baab589e08e2f1ce1aa55
wyvernbw <iamwyvern511@gmail.com>
2024-08-17 15:02:03 +0300

    add maybe

--- components/movable.gd
+++ components/movable.gd
@@ -1,1 +1,18 @@
 extends Node
+
+const GRAVITY := 900.0
+const GRAVITY_ACCEL := 160.0
+
+@export var use_gravity: bool = true
+@export var gravity: float = GRAVITY
+@export var gravity_accel: float = GRAVITY_ACCEL
+
+func _ready() -> void:
+	assert(owner is CharacterBody2D)
+
+func _physics_process(delta: float) -> void:
+	owner.velocity = owner.velocity.move_toward(
+		Vector2.DOWN * gravity, 
+		gravity_accel * delta
+	)
+	owner.move_and_slide()
--- entities/jon/jon.gd
+++ entities/jon/jon.gd
@@ -1,5 +1,29 @@
 extends CharacterBody2D
 
-class Idle:
+class MoveState:
 	extends Wisp.State
 
+class Idle:
+	extends MoveState
+
+class JumpState:
+	extends Wisp.State
+
+class NoJump: 
+	extends JumpState
+
+
+@onready var move_state := Wisp.use_state_machine(self, Idle.new())
+@onready var jump_state := Wisp.use_state_machine(self, NoJump.new())
+
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	move_state.input(event)
+	jump_state.input(event)
+
+func _process(delta: float) -> void:
+	move_state.process(delta)
+	jump_state.process(delta)
+
+func _physics_process(delta: float) -> void:
+	move_state.physics_process(delta)
+	jump_state.physics_process(delta)
